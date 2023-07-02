048a26bc866d50182504b8b64c47fcf3ca2d3982
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-02 11:48:11 +0530

    Version_1

---
+++ script.js
@@ -0,0 +1,6 @@
+let Items = document.getElementsByClassName('list-group-item');
+console.log(Items);
+Items[2].style.backgroundColor = "green";
+
+let listGroup = document.getElementById('items');
+listGroup.style.fontWeight = "bold";
